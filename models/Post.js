84a345ab833d11b9e83f19fc7eb314687ecad992
chuongcuu <dangchuong1999@outlook.com>
2019-12-13 02:16:01 +0200

    Failed - Route for delete comment.

--- models/Post.js
+++ models/Post.js
@@ -18,36 +18,32 @@ const PostSchema = new Schema({
         type: String,
         required: true
     },
-    likes: [
-        {
-            user: {
-                type: Schema.Types.ObjectId,
-                ref: 'users'
-            }
+    likes: [{
+        user: {
+            type: Schema.Types.ObjectId,
+            ref: 'users'
         }
-    ],
-    comments: [
-        {
-            user: {
-                type: Schema.Types.ObjectId,
-                ref: 'users'
-            },
-            text: {
-                type: String,
-                required: true
-            },
-            name: {
-                type: String
-            },
-            avatar: {
-                type: String
-            },
-            date: {
-                type: Date,
-                default: Date.now
-            }
+    }],
+    comments: [{
+        user: {
+            type: Schema.Types.ObjectId,
+            ref: 'users'
+        },
+        text: {
+            type: String,
+            required: true
+        },
+        name: {
+            type: String
+        },
+        avatar: {
+            type: String
+        },
+        date: {
+            type: Date,
+            default: Date.now
         }
-    ],
+    }],
     date: {
         type: Date,
         default: Date.now
