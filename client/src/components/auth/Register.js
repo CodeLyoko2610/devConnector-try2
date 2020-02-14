ee3efe5c9c9d332de62c016e6f8b9522674cf371
chuongcuu <dangchuong1999@outlook.com>
2020-02-14 14:08:50 +0200

    Register component: Add onSubmit event handler to test submitting value.

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -13,9 +13,18 @@ const Register = () => {
   //Destructuring formData
   let { name, email, password, password2 } = formData;
 
-  //Take in user input
+  //Take in user input and update these input in UI
   const onChange = e => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const onSubmit = e => {
+    e.preventDefault();
+    if (password !== password2) {
+      console.log('Incorect passwords. Please change it.');
+    } else {
+      console.log(formData);
+    }
   };
   return (
     <Fragment>
@@ -23,7 +32,7 @@ const Register = () => {
       <p className='lead'>
         <i className='fas fa-user'></i> Create Your Account
       </p>
-      <form className='form' action='create-profile.html'>
+      <form className='form' onSubmit={e => onSubmit(e)}>
         <div className='form-group'>
           <input
             type='text'
@@ -31,7 +40,7 @@ const Register = () => {
             name='name'
             required
             onChange={e => onChange(e)}
-            value={name}
+            value={name} //setting the value of this field to the state's value
           />
         </div>
         <div className='form-group'>
